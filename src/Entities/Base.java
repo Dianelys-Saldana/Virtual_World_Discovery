e374a3d4129b4da405117717a9be49c8ceee3894
Carlos <carlos.rodriguez124@upr.edu>
2020-03-06 17:14:20 -0400

    Player Added

---
+++ src/Entities/Base.java
@@ -0,0 +1,40 @@
+package Entities;
+
+import java.awt.Rectangle;
+
+public abstract class Base extends Rectangle{
+
+	private static final long serialVersionUID = 1L;
+	
+	private int speed;
+	private int direction;
+	
+	public Base() {}
+	
+	public Base(int xPos, int yPos, int width, int height) {
+		super(xPos, yPos,width, height);
+	}
+	
+	public int getPixelsWide() {
+		return (int) getWidth();
+	}
+	
+	public int getPixelsTall() {
+		return (int) getHeight();
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	
+	public int getDirection() {
+		return direction;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	public void setDirection(int direction) {
+		this.direction = direction;
+	}
+}
---
+++ src/Entities/Player.java
@@ -0,0 +1,31 @@
+package Entities;
+
+public class Player extends Base {
+	private static final long serialVersionUID = 1L;
+	
+	public static final int DEFAULT_SPEED = 5;
+	public static final int Y_OFFSET = 5; // initial y distance of the ship from the bottom of the screen 
+	
+	public static final int WIDTH = 42;
+	public static final int HEIGHT = 41;
+	//public static final int speed = DEFAULT_SPEED;
+	
+	public Player(int xPos, int yPos){
+		super(xPos, yPos, WIDTH, HEIGHT);
+		this.setSpeed(DEFAULT_SPEED);
+	}
+	
+	public int gtInitialYOffset() {
+		return Y_OFFSET;
+	}
+	
+	
+	/**
+	 * Returns the default ship speed.
+	 * @return the default ship speed
+	 */
+	public int getDefaultSpeed(){
+		return DEFAULT_SPEED;
+	}
+	
+}
